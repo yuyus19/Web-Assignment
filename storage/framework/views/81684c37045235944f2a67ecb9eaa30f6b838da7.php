f47ec116413534c3ceb0e3303b06f7022f9d7061
Yusuf Yusuf <56111176+yuyus19@users.noreply.github.com>
2022-09-27 11:56:38 +0200

    1
    1

---
+++ storage/framework/views/81684c37045235944f2a67ecb9eaa30f6b838da7.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="<?php echo e(asset('styles/primary.css')); ?>" />
+    <link rel="stylesheet" href="<?php echo e(asset('styles/custom.css')); ?>" />
+    <script src="<?php echo e(asset('js/add-to-cart.js')); ?>" defer></script>
+    <title>MyPC</title>
+</head>
+
+<body>
+    <nav>
+        <div class="container">
+            <a id="logo">
+                <h2>MyPC</h2>
+                <h3>WebStore</h3>
+            </a>
+            <div class="cart">
+                <span>In cart: </span><span id="cart-count"> 0</span>
+            </div>
+        </div>
+    </nav>
+    <section class="container">
+        <main>
+            <!--Your HTML-->
+        </main>
+    </section>
+</body>
+
+</html><?php /**PATH C:\Users\Yusuf\OneDrive\Documents\GitHub\yuyus19\resources\views/plebboi.blade.php ENDPATH**/ ?>
